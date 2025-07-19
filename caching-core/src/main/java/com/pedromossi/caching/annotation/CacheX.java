d162258e71a44f790daa6207455fe1d9c4e8c56a
Pedro Mossi <pedroluizmossi@gmail.com>
2025-07-19 01:21:55 -0300

    Import `CacheXAspect` in `CacheX` for enhanced functionality

--- caching-core/src/main/java/com/pedromossi/caching/annotation/CacheX.java
+++ caching-core/src/main/java/com/pedromossi/caching/annotation/CacheX.java
@@ -1,5 +1,7 @@
 package com.pedromossi.caching.annotation;
 
+import com.pedromossi.caching.aspect.CacheXAspect;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
